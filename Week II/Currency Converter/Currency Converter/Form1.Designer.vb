c3d3e12df1bbd5c3c100380b5087ff37e8535405
vshehu <vshehu@gmail.com>
2015-10-15 19:17:49 +0200

    Committed the Currency Converter from second weeks lecture
    Committed the Currency Converter from second weeks lecture

---
+++ Week II/Currency Converter/Currency Converter/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pctLogo = New System.Windows.Forms.PictureBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblAmount = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.cmbFrom = New System.Windows.Forms.ComboBox()
+        Me.cmbTo = New System.Windows.Forms.ComboBox()
+        Me.lblFrom = New System.Windows.Forms.Label()
+        Me.lblTo = New System.Windows.Forms.Label()
+        CType(Me.pctLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pctLogo
+        '
+        Me.pctLogo.Image = CType(resources.GetObject("pctLogo.Image"), System.Drawing.Image)
+        Me.pctLogo.Location = New System.Drawing.Point(12, 12)
+        Me.pctLogo.Name = "pctLogo"
+        Me.pctLogo.Size = New System.Drawing.Size(435, 62)
+        Me.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pctLogo.TabIndex = 23
+        Me.pctLogo.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(334, 134)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(113, 23)
+        Me.btnExit.TabIndex = 22
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(334, 106)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(113, 23)
+        Me.btnClear.TabIndex = 21
+        Me.btnClear.Text = "Cl&ear "
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(334, 80)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(113, 23)
+        Me.btnCalculate.TabIndex = 20
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblAmount
+        '
+        Me.lblAmount.AutoSize = True
+        Me.lblAmount.Location = New System.Drawing.Point(12, 90)
+        Me.lblAmount.Name = "lblAmount"
+        Me.lblAmount.Size = New System.Drawing.Size(43, 13)
+        Me.lblAmount.TabIndex = 24
+        Me.lblAmount.Text = "Amount"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(161, 82)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(167, 20)
+        Me.txtAmount.TabIndex = 25
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(161, 161)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.Size = New System.Drawing.Size(167, 20)
+        Me.txtResult.TabIndex = 27
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(12, 164)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(37, 13)
+        Me.lblResult.TabIndex = 26
+        Me.lblResult.Text = "Result"
+        '
+        'cmbFrom
+        '
+        Me.cmbFrom.FormattingEnabled = True
+        Me.cmbFrom.Items.AddRange(New Object() {"Denar", "Euro", "Dollar", "Swiss Franc"})
+        Me.cmbFrom.Location = New System.Drawing.Point(161, 107)
+        Me.cmbFrom.Name = "cmbFrom"
+        Me.cmbFrom.Size = New System.Drawing.Size(167, 21)
+        Me.cmbFrom.TabIndex = 28
+        '
+        'cmbTo
+        '
+        Me.cmbTo.FormattingEnabled = True
+        Me.cmbTo.Items.AddRange(New Object() {"Denar", "Euro", "Dollar", "Swiss Franc"})
+        Me.cmbTo.Location = New System.Drawing.Point(161, 134)
+        Me.cmbTo.Name = "cmbTo"
+        Me.cmbTo.Size = New System.Drawing.Size(167, 21)
+        Me.cmbTo.TabIndex = 29
+        '
+        'lblFrom
+        '
+        Me.lblFrom.AutoSize = True
+        Me.lblFrom.Location = New System.Drawing.Point(12, 116)
+        Me.lblFrom.Name = "lblFrom"
+        Me.lblFrom.Size = New System.Drawing.Size(30, 13)
+        Me.lblFrom.TabIndex = 30
+        Me.lblFrom.Text = "From"
+        '
+        'lblTo
+        '
+        Me.lblTo.AutoSize = True
+        Me.lblTo.Location = New System.Drawing.Point(12, 139)
+        Me.lblTo.Name = "lblTo"
+        Me.lblTo.Size = New System.Drawing.Size(20, 13)
+        Me.lblTo.TabIndex = 31
+        Me.lblTo.Text = "To"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 193)
+        Me.Controls.Add(Me.lblTo)
+        Me.Controls.Add(Me.lblFrom)
+        Me.Controls.Add(Me.cmbTo)
+        Me.Controls.Add(Me.cmbFrom)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.lblAmount)
+        Me.Controls.Add(Me.pctLogo)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Currency Converter"
+        CType(Me.pctLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pctLogo As System.Windows.Forms.PictureBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lblAmount As System.Windows.Forms.Label
+    Friend WithEvents txtAmount As System.Windows.Forms.TextBox
+    Friend WithEvents txtResult As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents cmbFrom As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbTo As System.Windows.Forms.ComboBox
+    Friend WithEvents lblFrom As System.Windows.Forms.Label
+    Friend WithEvents lblTo As System.Windows.Forms.Label
+
+End Class
---
+++ Week II/Currency Converter/Currency Converter/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+
+    Dim denarsToEuro As Decimal = 62.1
+    Dim denarsToDollar As Decimal = 54
+    Dim denarsToFranc As Decimal = 56.5
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbFrom.SelectedIndex = 0
+        cmbTo.SelectedIndex = 1
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim amount As Decimal
+
+        If Decimal.TryParse(txtAmount.Text, amount) = False Then
+            MessageBox.Show("Please use valid numbers for amount!")
+            txtAmount.Focus()
+            txtAmount.Text = ""
+            Return
+        End If
+       
+        Dim conversionRate As Decimal
+        If cmbFrom.SelectedIndex = 0 Then
+            If cmbTo.SelectedIndex = 0 Then
+                conversionRate = 1
+            ElseIf cmbTo.SelectedIndex = 1 Then
+                conversionRate = denarsToEuro
+            ElseIf cmbTo.SelectedIndex = 2 Then
+                conversionRate = denarsToDollar
+            Else
+                conversionRate = denarsToFranc
+            End If
+        End If
+
+        Dim result As Decimal
+        result = amount / conversionRate
+
+        txtResult.Text = result.ToString()
+
+    End Sub
+End Class
