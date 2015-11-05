416b1dc2d1407176a9628c9cc6b0a12780411a20
vshehu <vshehu@gmail.com>
2015-11-05 12:49:19 +0100

    Parking meter
    Week V application - Understanding Functions and Subs

---
+++ Week IV/Parking Meter/Parking Meter/Form1.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pctLogo = New System.Windows.Forms.PictureBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtHourlyRate = New System.Windows.Forms.TextBox()
+        Me.dtpEntrance = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.dtpExit = New System.Windows.Forms.DateTimePicker()
+        Me.cmbDiscount = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTotalWithoutVAT = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtVAT = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pctLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pctLogo
+        '
+        Me.pctLogo.Image = CType(resources.GetObject("pctLogo.Image"), System.Drawing.Image)
+        Me.pctLogo.Location = New System.Drawing.Point(12, 12)
+        Me.pctLogo.Name = "pctLogo"
+        Me.pctLogo.Size = New System.Drawing.Size(435, 62)
+        Me.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pctLogo.TabIndex = 27
+        Me.pctLogo.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(334, 134)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(113, 23)
+        Me.btnExit.TabIndex = 26
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(334, 106)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(113, 23)
+        Me.btnClear.TabIndex = 25
+        Me.btnClear.Text = "Cl&ear "
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(334, 80)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(113, 23)
+        Me.btnCalculate.TabIndex = 24
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(331, 244)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "Time:"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime.ForeColor = System.Drawing.Color.Red
+        Me.lblTime.Location = New System.Drawing.Point(375, 244)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(45, 13)
+        Me.lblTime.TabIndex = 29
+        Me.lblTime.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "Hourly rate"
+        '
+        'txtHourlyRate
+        '
+        Me.txtHourlyRate.Location = New System.Drawing.Point(119, 83)
+        Me.txtHourlyRate.Name = "txtHourlyRate"
+        Me.txtHourlyRate.Size = New System.Drawing.Size(180, 20)
+        Me.txtHourlyRate.TabIndex = 31
+        '
+        'dtpEntrance
+        '
+        Me.dtpEntrance.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.dtpEntrance.Location = New System.Drawing.Point(119, 110)
+        Me.dtpEntrance.Name = "dtpEntrance"
+        Me.dtpEntrance.Size = New System.Drawing.Size(180, 20)
+        Me.dtpEntrance.TabIndex = 32
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 116)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 33
+        Me.Label4.Text = "Entrance time"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 139)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 13)
+        Me.Label5.TabIndex = 35
+        Me.Label5.Text = "Exit time"
+        '
+        'dtpExit
+        '
+        Me.dtpExit.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.dtpExit.Location = New System.Drawing.Point(119, 136)
+        Me.dtpExit.Name = "dtpExit"
+        Me.dtpExit.Size = New System.Drawing.Size(180, 20)
+        Me.dtpExit.TabIndex = 34
+        '
+        'cmbDiscount
+        '
+        Me.cmbDiscount.FormattingEnabled = True
+        Me.cmbDiscount.Items.AddRange(New Object() {"General public (0 %)", "Students (100 %)", "Employees (50 %)"})
+        Me.cmbDiscount.Location = New System.Drawing.Point(119, 162)
+        Me.cmbDiscount.Name = "cmbDiscount"
+        Me.cmbDiscount.Size = New System.Drawing.Size(180, 21)
+        Me.cmbDiscount.TabIndex = 36
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 165)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 37
+        Me.Label6.Text = "Discount"
+        '
+        'txtTotalWithoutVAT
+        '
+        Me.txtTotalWithoutVAT.Location = New System.Drawing.Point(119, 189)
+        Me.txtTotalWithoutVAT.Name = "txtTotalWithoutVAT"
+        Me.txtTotalWithoutVAT.ReadOnly = True
+        Me.txtTotalWithoutVAT.Size = New System.Drawing.Size(180, 20)
+        Me.txtTotalWithoutVAT.TabIndex = 39
+        Me.txtTotalWithoutVAT.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 192)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 13)
+        Me.Label7.TabIndex = 38
+        Me.Label7.Text = "Total without tax"
+        '
+        'txtVAT
+        '
+        Me.txtVAT.Location = New System.Drawing.Point(119, 215)
+        Me.txtVAT.Name = "txtVAT"
+        Me.txtVAT.ReadOnly = True
+        Me.txtVAT.Size = New System.Drawing.Size(180, 20)
+        Me.txtVAT.TabIndex = 41
+        Me.txtVAT.TabStop = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 218)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(25, 13)
+        Me.Label8.TabIndex = 40
+        Me.Label8.Text = "Tax"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(119, 241)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(180, 20)
+        Me.txtTotal.TabIndex = 43
+        Me.txtTotal.TabStop = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 244)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(31, 13)
+        Me.Label9.TabIndex = 42
+        Me.Label9.Text = "Total"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(457, 269)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtVAT)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtTotalWithoutVAT)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.cmbDiscount)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.dtpExit)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.dtpEntrance)
+        Me.Controls.Add(Me.txtHourlyRate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pctLogo)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Parking Meter"
+        CType(Me.pctLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pctLogo As System.Windows.Forms.PictureBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtHourlyRate As System.Windows.Forms.TextBox
+    Friend WithEvents dtpEntrance As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents dtpExit As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cmbDiscount As System.Windows.Forms.ComboBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtTotalWithoutVAT As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txtVAT As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents txtTotal As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Week IV/Parking Meter/Parking Meter/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblTime.Text = Now.ToLongTimeString()
+    End Sub
+
+  
+    Function CalculateTotalWithoutVAT(ByVal hours As Integer, ByVal rate As Decimal, ByVal discount As Integer) As Decimal
+
+        Dim totalWithoutDiscount As Decimal
+        totalWithoutDiscount = hours * rate
+
+        Dim totalWithDiscount As Decimal
+        totalWithDiscount = totalWithoutDiscount - (totalWithDiscount * discount / 100)
+
+        Return totalWithDiscount
+    End Function
+
+    Function CalculateTotalWithVAT(ByVal hours As Integer, ByVal rate As Decimal, ByVal discount As Integer) As Decimal
+
+        Dim totalWithoutVat As Decimal = CalculateTotalWithoutVAT(hours, rate, discount)
+
+        totalWithoutVat = totalWithoutVat + (totalWithoutVat * 18 / 100)
+
+        Return totalWithoutVat
+
+    End Function
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim rate As Integer
+        Dim discount As Integer
+        Dim hours As Integer
+
+        rate = Convert.ToInt32(txtHourlyRate.Text)
+        hours = dtpExit.Value.Subtract(dtpEntrance.Value).Hours
+
+        If cmbDiscount.SelectedIndex = 0 Then
+            discount = 0
+        ElseIf cmbDiscount.SelectedIndex = 1 Then
+            discount = 100
+        Else
+            discount = 50
+        End If
+
+        txtTotalWithoutVAT.Text = CalculateTotalWithoutVAT(hours, rate, discount)
+
+        txtTotal.Text = CalculateTotalWithVAT(hours, rate, discount)
+    End Sub
+End Class
