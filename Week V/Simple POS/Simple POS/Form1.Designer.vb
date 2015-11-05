77636892cc5847fd79cad34801232b97e910ab23
vshehu <vshehu@gmail.com>
2015-11-05 12:45:39 +0100

    Simple POS application added
    Week V application. How to work with arrays and parallel arrays (See
    chapter 9 on the book)

---
+++ Week V/Simple POS/Simple POS/Form1.Designer.vb
@@ -0,0 +1,327 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pctLogo = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSearchId = New System.Windows.Forms.TextBox()
+        Me.txtSearchName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSearchById = New System.Windows.Forms.Button()
+        Me.btnSearchByName = New System.Windows.Forms.Button()
+        Me.txtProductName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtVAT = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtAmountStock = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lstSearchedProducts = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        CType(Me.pctLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pctLogo
+        '
+        Me.pctLogo.Image = CType(resources.GetObject("pctLogo.Image"), System.Drawing.Image)
+        Me.pctLogo.Location = New System.Drawing.Point(12, 12)
+        Me.pctLogo.Name = "pctLogo"
+        Me.pctLogo.Size = New System.Drawing.Size(435, 62)
+        Me.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pctLogo.TabIndex = 28
+        Me.pctLogo.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 93)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 29
+        Me.Label1.Text = "Search by Id"
+        '
+        'txtSearchId
+        '
+        Me.txtSearchId.Location = New System.Drawing.Point(146, 90)
+        Me.txtSearchId.Name = "txtSearchId"
+        Me.txtSearchId.Size = New System.Drawing.Size(158, 20)
+        Me.txtSearchId.TabIndex = 30
+        '
+        'txtSearchName
+        '
+        Me.txtSearchName.Location = New System.Drawing.Point(146, 116)
+        Me.txtSearchName.Name = "txtSearchName"
+        Me.txtSearchName.Size = New System.Drawing.Size(159, 20)
+        Me.txtSearchName.TabIndex = 32
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 13)
+        Me.Label2.TabIndex = 31
+        Me.Label2.Text = "Search by part of name"
+        '
+        'btnSearchById
+        '
+        Me.btnSearchById.Location = New System.Drawing.Point(311, 88)
+        Me.btnSearchById.Name = "btnSearchById"
+        Me.btnSearchById.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearchById.TabIndex = 33
+        Me.btnSearchById.Text = "Search"
+        Me.btnSearchById.UseVisualStyleBackColor = True
+        '
+        'btnSearchByName
+        '
+        Me.btnSearchByName.Location = New System.Drawing.Point(311, 114)
+        Me.btnSearchByName.Name = "btnSearchByName"
+        Me.btnSearchByName.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearchByName.TabIndex = 34
+        Me.btnSearchByName.Text = "Search"
+        Me.btnSearchByName.UseVisualStyleBackColor = True
+        '
+        'txtProductName
+        '
+        Me.txtProductName.Location = New System.Drawing.Point(16, 165)
+        Me.txtProductName.Name = "txtProductName"
+        Me.txtProductName.ReadOnly = True
+        Me.txtProductName.Size = New System.Drawing.Size(158, 20)
+        Me.txtProductName.TabIndex = 36
+        Me.txtProductName.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 35
+        Me.Label3.Text = "Product name"
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(190, 165)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.ReadOnly = True
+        Me.txtPrice.Size = New System.Drawing.Size(79, 20)
+        Me.txtPrice.TabIndex = 38
+        Me.txtPrice.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(198, 149)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
+        Me.Label4.TabIndex = 37
+        Me.Label4.Text = "Price"
+        '
+        'txtVAT
+        '
+        Me.txtVAT.Location = New System.Drawing.Point(275, 165)
+        Me.txtVAT.Name = "txtVAT"
+        Me.txtVAT.ReadOnly = True
+        Me.txtVAT.Size = New System.Drawing.Size(79, 20)
+        Me.txtVAT.TabIndex = 40
+        Me.txtVAT.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(283, 149)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 13)
+        Me.Label5.TabIndex = 39
+        Me.Label5.Text = "VAT"
+        '
+        'txtAmountStock
+        '
+        Me.txtAmountStock.Location = New System.Drawing.Point(360, 165)
+        Me.txtAmountStock.Name = "txtAmountStock"
+        Me.txtAmountStock.ReadOnly = True
+        Me.txtAmountStock.Size = New System.Drawing.Size(84, 20)
+        Me.txtAmountStock.TabIndex = 42
+        Me.txtAmountStock.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(357, 149)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(87, 13)
+        Me.Label6.TabIndex = 41
+        Me.Label6.Text = "Amount on stock"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTotal.Location = New System.Drawing.Point(539, 165)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(84, 20)
+        Me.txtTotal.TabIndex = 44
+        Me.txtTotal.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(540, 149)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(36, 13)
+        Me.Label7.TabIndex = 43
+        Me.Label7.Text = "Total"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(629, 163)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(52, 23)
+        Me.btnAdd.TabIndex = 45
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(450, 165)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(84, 20)
+        Me.txtAmount.TabIndex = 47
+        Me.txtAmount.Text = "1"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(447, 149)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(43, 13)
+        Me.Label8.TabIndex = 46
+        Me.Label8.Text = "Amount"
+        '
+        'lstSearchedProducts
+        '
+        Me.lstSearchedProducts.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
+        Me.lstSearchedProducts.Location = New System.Drawing.Point(16, 192)
+        Me.lstSearchedProducts.Name = "lstSearchedProducts"
+        Me.lstSearchedProducts.Size = New System.Drawing.Size(665, 97)
+        Me.lstSearchedProducts.TabIndex = 48
+        Me.lstSearchedProducts.UseCompatibleStateImageBehavior = False
+        Me.lstSearchedProducts.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Id"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Product name"
+        Me.ColumnHeader2.Width = 121
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Product Price"
+        Me.ColumnHeader3.Width = 127
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "VAT"
+        Me.ColumnHeader4.Width = 109
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Amount on stock"
+        Me.ColumnHeader5.Width = 184
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(686, 479)
+        Me.Controls.Add(Me.lstSearchedProducts)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtAmountStock)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtVAT)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtProductName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSearchByName)
+        Me.Controls.Add(Me.btnSearchById)
+        Me.Controls.Add(Me.txtSearchName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSearchId)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pctLogo)
+        Me.Name = "Form1"
+        Me.Text = "Simple POS"
+        CType(Me.pctLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pctLogo As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtSearchId As System.Windows.Forms.TextBox
+    Friend WithEvents txtSearchName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnSearchById As System.Windows.Forms.Button
+    Friend WithEvents btnSearchByName As System.Windows.Forms.Button
+    Friend WithEvents txtProductName As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtPrice As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtVAT As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtAmountStock As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtTotal As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents txtAmount As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents lstSearchedProducts As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ Week V/Simple POS/Simple POS/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    'ProductNames contains information about the id of the product (first column of the array) and the name of the product (second column of the array)
+    Dim productNames(,) As String = {{"1", "Coca Cola"}, {"2", "Pepsi"}, {"3", "Bread"}, {"4", "Suggar"}, {"5", "Milk"}}
+    'ProductPrices contains information about product price (first column) ammount on stock (second column) and VAT (third column). VAT can be 18% or 5 %
+    Dim productPrices(,) As Decimal = {{75, 23, 18}, {70, 103, 18}, {25, 17, 5}, {50, 100, 5}, {55, 45, 5}}
+
+    ''' <summary>
+    ''' This function will search the array for a product based on 
+    ''' part of the product name
+    ''' </summary>
+    ''' <param name="productName">The name of the product 
+    ''' being searched
+    ''' </param>
+    ''' <returns>The indexes of all products found or an 
+    ''' empty array if nothing is found</returns>
+    ''' <remarks></remarks>
+    Function SearchProductByName(ByVal productName As String) As Integer()
+
+        Dim lst As New List(Of Integer)
+
+        Dim nrElements As Integer = productNames.GetUpperBound(0)
+
+        For i As Integer = 0 To nrElements
+            If productNames(i, 1).Contains(productName) Then
+                lst.Add(i)
+            End If
+        Next
+
+        Return lst.ToArray()
+
+    End Function
+    ''' <summary>
+    ''' This function will search the array for a product based on 
+    ''' the id of the product
+    ''' </summary>
+    ''' <param name="productId">The id of the product</param>
+    ''' <returns>The index of the found element on the product 
+    ''' array or -1 if nothing is found</returns>
+    ''' <remarks></remarks>
+    Function SearchProductById(ByVal productId As String) As Integer
+        Dim nrElements As Integer = productNames.GetUpperBound(0)
+
+        For i As Integer = 0 To nrElements
+            If productNames(i, 0) = productId Then
+                Return i
+            End If
+        Next
+        Return -1
+    End Function
+
+    Private Sub btnSearchById_Click(sender As Object, e As EventArgs) Handles btnSearchById.Click
+        Dim index As Integer = SearchProductById(txtSearchId.Text)
+
+        If index = -1 Then
+            MessageBox.Show("No product found with the given Id!")
+            Return
+        End If
+
+        txtProductName.Text = productNames(index, 1)
+        txtAmountStock.Text = productPrices(index, 1)
+        txtPrice.Text = productPrices(index, 0)
+        txtVAT.Text = productPrices(index, 2)
+
+    End Sub
+
+    Private Sub btnSearchByName_Click(sender As Object, e As EventArgs) Handles btnSearchByName.Click
+        Dim indexes() As Integer = SearchProductByName(txtSearchName.Text)
+
+        lstSearchedProducts.Items.Clear()
+
+        For Each index As Integer In indexes
+            Dim lvi As New ListViewItem
+            'get the id of the product
+            lvi.Text = productNames(index, 0)
+            'get the name of the product
+            lvi.SubItems.Add(productNames(index, 1))
+            'get the price of the product
+            lvi.SubItems.Add(productPrices(index, 0))
+            'get the VAT of the product
+            lvi.SubItems.Add(productPrices(index, 2))
+            'get the amount of the product
+            lvi.SubItems.Add(productPrices(index, 1))
+
+            'Add this row in the searched product list
+            lstSearchedProducts.Items.Add(lvi)
+        Next
+    End Sub
+End Class
